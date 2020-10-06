1d876050c6fb99d1878621efc7fcfb2bf55c9b66
Kevin <kphourat@gmail.com>
2020-10-06 21:01:33 +0200

    Add simple list in main activity

--- app/src/main/java/com/cmy/tic_cash_android/MainActivity.kt
+++ app/src/main/java/com/cmy/tic_cash_android/MainActivity.kt
@@ -2,11 +2,18 @@ package com.cmy.tic_cash_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myStringArray = arrayOf("a", "b", "c")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray)
+        val listView: ListView = findViewById(R.id.listview)
+        listView.adapter = adapter
     }
 }
